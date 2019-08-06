dca033d6c6d42bc11e25d7c8922de84eccbd0613
jeffshrager <jshrager@stanford.edu>
2019-08-05 20:24:10 -0700

    Bigger world, past two updates were dirt and trees.

--- leo.py
+++ leo.py
@@ -2,7 +2,7 @@ from numpy import *
 from random import * 
 
 height = 20
-width = 50
+width = 75
 world=ndarray(shape=(height,width), dtype=integer)
 gl=int(floor(height/2)) # Ground level
 
@@ -146,3 +146,41 @@ def ana(a):
 
 
 #ana(atree)
+
+#experimenting with curses
+
+"""
+
+# Draw text to center of screen
+import curses
+
+screen = curses.initscr()
+num_rows, num_cols = screen.getmaxyx()
+
+# Make a function to print a line in the center of screen
+def print_center(message):
+    # Calculate center row
+    middle_row = int(num_rows / 2)
+
+    # Calculate center column, and then adjust starting position based
+    # on the length of the message
+    half_length_of_message = int(len(message) / 2)
+    middle_column = int(num_cols / 2)
+    x_position = middle_column - half_length_of_message
+
+    # Draw the text
+    screen.addstr(middle_row, x_position, message)
+    screen.refresh()
+
+    # Now go into a loop that read keystrokes and display the characters typed in
+    # a random location on the screen, until 'z' is typed, at which point you stop
+    while True:
+        v=screen.getch()
+        screen.addch(randint(1,num_rows-1),randint(1,num_cols-1),v)
+        if v=='zz':
+            break
+
+
+print_center("Hello from the center!")
+curses.endwin()
+"""
