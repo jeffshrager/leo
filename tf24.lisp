93fa41d7baad7ef6e77e6c9b0511976c27f5bca0
jeffshrager <jshrager@stanford.edu>
2020-08-27 17:14:50 -0700

    no message

--- tf24.lisp
+++ tf24.lisp
@@ -2,14 +2,26 @@
 
 (defparameter *ops* '(* - + / expt))
 (defparameter *lops* (length *ops*))
-;;(defparameter *ns* '(1 2 3 4))
-(defparameter *ns* '(1 1 1 1))
+(defparameter *ns* '(2 4 9 12))
+;;(defparameter *ns* '(1 1 1 1))
 (defparameter *lns* (length *ns*))
-(defparameter *depth-limit* 5)
+(defparameter *depth-limit* 4)
+(defvar *value->expr* (make-hash-table :test #'equal))
+(defvar *expr->value* (make-hash-table :test #'equal))
 
 (defun run ()
-  (loop for target below 100
+  (loop for target from 20 to 25
 	do (find-expr target)))
+
+(defun run24 ()
+  (clrhash *expr->value*)
+  (loop for i below 100
+	as x = (find-expr 24)
+	unless (gethash x *expr->value*)
+	do
+	(print x)
+	(setf (gethash x *expr->value*) t)
+	))
 
 (defun find-expr (target)
   ;;(format t "~% ======= ~a~%" target)
@@ -17,7 +29,8 @@
 	as ntries = (expt 10 power)
 	as (shortest . minlength) = (find-expr-in-n-tries target ntries)
 	until shortest
-	finally (format t "~a (~a) ~a [~a]~%" target minlength shortest power)))
+	finally (progn (format t "~a (~a) ~a [~a]~%" target minlength shortest power)
+		       (return shortest))))
 
 (defun find-expr-in-n-tries (target ntries)
   (loop as try below ntries
@@ -42,7 +55,7 @@
   (let ((flat-expr (flatten expr)))
     (and 
      (<= (count 'expt flat-expr) 1) ;; or it'll loop with huge numbers!
-     ;;(uses-all-only-once flat-expr *ns*)
+     (uses-all-only-once flat-expr *ns*)
      ;;(uses-all-only-once flat-expr *ops*)
      )))
 
@@ -61,4 +74,40 @@
 		 (an-expr (1- depth-limit))
 		 (an-expr (1- depth-limit))))))
 
-(run)
+#|
+
+(defun exprs (d)
+  (cond ((zerop d) nil)
+	(t (append (copy-seq *ns*)
+		   (let ((xs (exprs (1- d))))
+ 		     (xspread xs xs))))))
+(defun xspread (x1 x2)
+  (loop for e1 in x1
+	append (loop for e2 in x2
+		     append (loop for op in *ops*
+				  collect `(,op ,e1 ,e2)))))
+
+(defvar *value->expr* (make-hash-table :tests #'equal))
+(defvar *expr->value* (make-hash-table :tests #'equal))
+
+(defun run (&aux expers (exprs *depth-limit*))
+  (clrhash *value->expr*)
+  (clrhash *expr->value*)
+  (loop for v below 100
+	as x = (gethash v *value->expr*)
+	if x
+	do (return x)
+	else
+	do (loop for x in (exprs *depth-limit*)
+		 as v2 = (gethash x *expr->value*)
+		 if (= v2 v)
+		 do (return x)
+		 else (let ((v (ignore-errors (eval x))))
+			(when v
+			    (progn
+			      (setf (gethash x *expr->value*) v
+				    (gethash v *value->expr*) x)))))))
+			  
+|#
+(run24)
+
